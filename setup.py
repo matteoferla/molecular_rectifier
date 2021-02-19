8aa94097692a68a7e1bfac306b1e757b98fb11d8
matteoferla <matteo.ferla@gmail.com>
2021-02-19 09:57:06 +0000

    :pencil: description for pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from warnings import warn
 from importlib import util
-import sys
+import sys, os
 
 if sys.version_info.major != 3 or sys.version_info.minor < 6:
     print(sys.version_info)
@@ -10,19 +10,29 @@ if sys.version_info.major != 3 or sys.version_info.minor < 6:
 # ---------- Non pip modules  ------------------------------------------------------------------------------------------
 
 if not util.find_spec('rdkit'):
-    raise ModuleNotFoundError('This 3.6+ script **requires** rdkit which cannot be pip installed.' +
+    warn('This 3.6+ script **requires** rdkit which cannot be pip installed.' +
                               ' To install try either ' +
                               'conda install -c conda-forge rdkit or ' +
                               'sudo apt-get/brew install python3-rdkit or visit rdkit documentation.')
 
+# ------------ description ---------------------------------------------------------------------------------------------
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    __doc__ = f.read()
+descr = __doc__.split('\n')[1] # non-title line.
+
+
 setup(
     name='molecular_rectifier',
     version='0.1.1',
+    python_requires='>3.6',
     packages=find_packages(),
     url='https://github.com/matteoferla/molecular_rectifier',
     license='MIT',
     author='Matteo Ferla',
     author_email='matteo.ferla@gmail.com',
-    description='Given an RDKit molecule that does not sanitise, correct it until it does, '+
-                'regardless of the severity of the change.'
+    description=descr,
+    long_description=__doc__,
+    long_description_content_type='text/markdown',
 )
